afacf38fa5c74fdd5471ed6f4308c9b3df8fb2de
Ibenshniben <151071452+Ibenshniben@users.noreply.github.com>
2025-03-13 22:38:18 +0100

    endringer som faen

---
+++ next.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  images: {
+    domains: ['your-domain.com'], // Add your image domains here if needed
+  },
+}
+
+module.exports = nextConfig
